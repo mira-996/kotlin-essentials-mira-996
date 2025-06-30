e633eec44606308cfc815a9a9a03de5e863d525f
mira-996 <q.miraa@gmail.com>
2025-07-01 00:10:19 +0400

    Completed main function with arg and println

--- src/main/kotlin/com/motycka/edu/lesson04/LoggingExample.kt
+++ src/main/kotlin/com/motycka/edu/lesson04/LoggingExample.kt
@@ -4,6 +4,14 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 
 private val logger = KotlinLogging.logger { }
 
+class UserService {
+    private val logger = KotlinLogging.logger {}
+
+    fun login(username: String) {
+        logger.info { "User $username attempted login" }
+    }
+}
+
 fun main() {
     logger.error { "error message" }
     logger.warn { "warn message" }
--- src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
+++ src/main/kotlin/com/motycka/edu/lesson04/TemperatureConverter.kt
@@ -8,7 +8,8 @@ private val logger = KotlinLogging.logger { }
  * Utility object for converting temperature values between Fahrenheit and Celsius
  */
 object TemperatureConverter {
-
+    private const val ABSOLUTE_ZERO_C = -273.15
+    private const val ABSOLUTE_ZERO_F = -459.67
     /**
      * Converts temperature value given in Fahrenheit to Celsius
      *
@@ -17,9 +18,14 @@ object TemperatureConverter {
      * @see <a href="https://en.wikipedia.org/wiki/Fahrenheit">Fahrenheit</a>
      * @see <a href="https://en.wikipedia.org/wiki/Celsius">Celsius</a>
      */
-    fun toCelsius(fahrenheit: Double): Double {
-        logger.info { "Converting $fahrenheit Fahrenheit to Celsius" }
-        return (fahrenheit - 32) * 5 / 9
+    fun toCelsius(fahrenheit: Double): Double? {
+        if (fahrenheit < ABSOLUTE_ZERO_F) {
+            logger.warn { "Fahrenheit value $fahrenheit is below absolute zero ($ABSOLUTE_ZERO_F°F)." }
+            return null
+        }
+        val result = (fahrenheit - 32) * 5 / 9
+        logger.info { "Converted $fahrenheit°F to $result°C" }
+        return result
     }
 
     /**
@@ -30,9 +36,14 @@ object TemperatureConverter {
      * @see <a href="https://en.wikipedia.org/wiki/Fahrenheit">Fahrenheit</a>
      * @see <a href="https://en.wikipedia.org/wiki/Celsius">Celsius</a>
      */
-    fun toFahrenheit(celsius: Double): Double {
-        logger.info { "Converting $celsius Celsius to Fahrenheit" }
-        return celsius * 9 / 5 + 32
+    fun toFahrenheit(celsius: Double): Double? {
+        if (celsius < ABSOLUTE_ZERO_C) {
+            logger.warn { "Celsius value $celsius is below absolute zero ($ABSOLUTE_ZERO_C°C)." }
+            return null
+        }
+        val result = celsius * 9 / 5 + 32
+        logger.info { "Converted $celsius°C to $result°F" }
+        return result
     }
 
 }
@@ -42,3 +53,4 @@ fun main() {
     TemperatureConverter.toFahrenheit(34.0).also(::println)
     TemperatureConverter.toCelsius(94.0).also(::println)
 }
+
